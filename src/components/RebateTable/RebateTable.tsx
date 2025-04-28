70c3c607a955b9b3a27edc96c87a2dba2d441338
Phuc <pnguyenxuan@zuelligpharma.com>
2025-04-28 17:36:46 +0700

    sort and group query data

--- src/components/RebateTable/RebateTable.tsx
+++ src/components/RebateTable/RebateTable.tsx
@@ -12,38 +12,113 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React, { useCallback, useEffect, useContext, useState } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { Box, Table, TableHead, TableHeaderCell, TableBody, TableRow, TableDataCell } from '@looker/components';
 import { ExtensionContext40 } from '@looker/extension-sdk-react';
 import './RebateTable.scss';
 
 type Field = {
   label: string;
-  name?: string;
+  name: string;
+  isCustom?: boolean;
 };
 
-const extraFields: Field[] = [
+type DataItem = {
+  name: string;
+  value: any;
+  rendered: any;
+  rowSpan: number;
+};
+
+const customFields: Field[] = [
   {
     label: 'FG/CD',
+    name: 'fg_cd',
+    isCustom: true,
   },
   {
     label: 'Rebate Product Qty',
+    name: 'rebate_product_qty',
+    isCustom: true,
   },
   {
     label: 'Selling Price',
+    name: 'selling_price',
+    isCustom: true,
   },
   {
     label: 'Rebate Amount',
+    name: 'rebate_amount',
+    isCustom: true,
   },
   {
     label: 'Balance',
+    name: 'balance',
+    isCustom: true,
   },
 ];
+
+function sortAndGroupQueryData(data: any[], fields: Field[]): DataItem[][] {
+  const sortedItems = data.sort((a, b) => {
+    const ka = a[fields[0].name].value + '_' + a[fields[2].name].value;
+    const kb = b[fields[0].name].value + '_' + b[fields[2].name].value;
+    return ka.localeCompare(kb);
+  });
+  let lastCus = '';
+  let lastCat = '';
+  const rowSpanMap: { [key: string]: number } = {
+    cus: 0,
+    cat: 0,
+  };
+
+  const result: DataItem[][] = [];
+
+  sortedItems.forEach((item, index) => {
+    const cus = item[fields[0].name].value;
+    const cat = item[fields[2].name].value;
+
+    if (cus !== lastCus) {
+      if (index > 0) {
+        result[index - rowSpanMap['cus']][0].rowSpan = rowSpanMap['cus'];
+        result[index - rowSpanMap['cus']][1].rowSpan = rowSpanMap['cus'];
+      }
+      rowSpanMap['cus'] = 1;
+      lastCus = cus;
+      lastCat = '';
+    } else {
+      rowSpanMap['cus']++;
+    }
+
+    if (cat !== lastCat) {
+      if (index > 0) {
+        result[index - rowSpanMap['cat']][2].rowSpan = rowSpanMap['cat'];
+      }
+      rowSpanMap['cat'] = 1;
+      lastCat = cat;
+    } else {
+      rowSpanMap['cat']++;
+    }
+
+    const values = fields.map((f, i) => ({
+      name: f.name,
+      value: item[f.name].value,
+      rendered: item[f.name].rendered || item[f.name].value,
+      rowSpan: i > 2 ? 1 : 0,
+    }));
+
+    result.push(values);
+  });
+
+  result[sortedItems.length - rowSpanMap['cus']][0].rowSpan = rowSpanMap['cus'];
+  result[sortedItems.length - rowSpanMap['cus']][1].rowSpan = rowSpanMap['cus'];
+  result[sortedItems.length - rowSpanMap['cat']][2].rowSpan = rowSpanMap['cat'];
+  return result;
+}
 
 const RebateTable = () => {
   const { extensionSDK, visualizationData } = useContext(ExtensionContext40);
   const [fields, setFields] = useState<Field[]>([]);
-  const [queryData, setQueryData] = useState<any[]>([]);
+  const [queryData, setQueryData] = useState<DataItem[][]>([]);
 
   useEffect(() => {
     extensionSDK.rendered();
@@ -51,36 +126,37 @@ const RebateTable = () => {
 
   useEffect(() => {
     if (!visualizationData?.queryResponse) return;
-    setFields(
-      [
-        ...visualizationData.queryResponse.fields['dimensions'],
-        ...visualizationData.queryResponse.fields['measures'],
-      ].map((item) => ({
+    const displayedFields = [
+      ...visualizationData.queryResponse.fields['dimensions'],
+      ...visualizationData.queryResponse.fields['measures'],
+    ]
+      .map((item) => ({
         label: item['label_short'],
         name: item['name'],
-      })),
-    );
-    setQueryData(visualizationData.queryResponse.data);
+      }))
+      .concat(customFields);
+    setFields(displayedFields);
+    setQueryData(sortAndGroupQueryData(visualizationData.queryResponse.data, displayedFields));
   }, [visualizationData]);
 
   return (
     <Box p="u4">
       <Table className="rebate-table">
         <TableHead>
-          {[...fields, ...extraFields].map((f) => (
+          {[...fields, ...customFields].map((f) => (
             <TableHeaderCell p="u1" border>
               {f.label}
             </TableHeaderCell>
           ))}
         </TableHead>
         <TableBody fontSize={'small'}>
-          {queryData.map((item) => (
+          {queryData.map((dataItems) => (
             <TableRow>
-              {fields.map(
-                (f) =>
-                  f.name && (
-                    <TableDataCell border p="u1">
-                      {item[f.name]?.rendered || item[f.name]?.value}
+              {dataItems.map(
+                (item) =>
+                  item.rowSpan && (
+                    <TableDataCell border p="u1" rowSpan={item.rowSpan}>
+                      {item.rendered}
                     </TableDataCell>
                   ),
               )}
